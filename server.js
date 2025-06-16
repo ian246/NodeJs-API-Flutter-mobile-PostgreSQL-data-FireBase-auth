70fc3140f6a1323b0dfb25c4eea7b44aaf8dae06
Ian Karel Ctvrtnik <ianctvrtnik@gmail.com>
2025-06-16 15:56:05 -0300

    TesteComit on Render

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import * as db from "./database-postgres.js";
-import { userSchema } from "./validations/user.schema.js";
-import { clientSchema } from "./validations/client.schema.js";
+import { userSchema } from "./src/validations/user.schema.js";
+import { clientSchema } from "./src/validations/client.schema.js";
 
 dotenv.config();
 
