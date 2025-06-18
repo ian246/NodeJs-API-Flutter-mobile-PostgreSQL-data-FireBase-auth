bd57da9da11682f6ea072c3c29e83fed4a3c2f51
Ian Karel Ctvrtnik <ianctvrtnik@gmail.com>
2025-06-17 22:20:18 -0300

    firebase.js to rend.com

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "./src/config/serviceAccountKey.json";
+import serviceAccount from "./src/config/serviceAccount.js";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
